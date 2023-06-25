395d9c5c31774bb6cadaba9962ddd488038aee1b
Anbu <anburuku@gmail.com>
2023-06-25 13:09:18 +0530

    bootcamp

--- src/main/java/automate_dashboard_functionality/EditDashboard.java
+++ src/main/java/automate_dashboard_functionality/EditDashboard.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class CreateNewDashboard {
+public class EditDashboard {
 	public static void main(String[] args) throws InterruptedException {
 		ChromeOptions options=new ChromeOptions();
 		options.addArguments("--disable-notifications");
